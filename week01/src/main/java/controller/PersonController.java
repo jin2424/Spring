568610deb58dc9e53c7c98fce02ad0fce2a7eb55
jins <rhrhkdwls24@naver.com>
2022-05-31 01:47:35 +0900

    person

---
+++ week01/src/main/java/controller/PersonController.java
@@ -0,0 +1,19 @@
+package controller;
+
+import com.sparta.week01.prac.Person;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PersonController {
+
+    @GetMapping("/myinfo")
+    public Person getPerson() {
+        Person person = new Person();
+        person.setName("손흥민");
+        person.setAddress("런던");
+        person.setAge(28);
+        person.setJob("대한민국 축구선수");
+        return person;
+    }
+}
