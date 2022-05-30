fd1c780442fa7fb04c15f7f901137130275012a6
jins <rhrhkdwls24@naver.com>
2022-05-30 21:36:51 +0900

    가위바위보

---
+++ week01/src/main/java/Main.java
@@ -0,0 +1,28 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+
+        Random random = new Random();
+        // 3개의 값( 가위바위보) +1은 0이 아닌 1부터 시작하기위해
+        int cpu = random.nextInt(3)+1;
+        System.out.println("cpu: "+cpu);
+
+        System.out.println("가위(1), 바위(2), 보(3): ");
+        Scanner sc = new Scanner(System.in);
+        int user = sc.nextInt();
+
+        if (cpu == user){
+            System.out.println("비겼습니다");
+        }else if (cpu > user) {
+            System.out.print("졌습니다");
+        } else {
+            System.out.print("이겼습니다");
+        }
+
+    }
+
+}
+
+
