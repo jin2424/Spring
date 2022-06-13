b1a99c0ff0ea40d5e63d60a56806c89dff651bde
KGJ24 <rhrhkdwls24@naver.com>
2022-06-14 00:51:30 +0900

    newweek

---
+++ newweek03/src/main/java/com/sparta/newweek03/Newweek03Application.java
@@ -0,0 +1,13 @@
+package com.sparta.newweek03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Newweek03Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Newweek03Application.class, args);
+    }
+
+}
---
+++ newweek03/src/test/java/com/sparta/newweek03/Newweek03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.newweek03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Newweek03ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
