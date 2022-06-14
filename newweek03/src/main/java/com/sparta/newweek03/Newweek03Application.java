948af51ad41bd2d5b131f6d8dae380e65220c957
KGJ24 <rhrhkdwls24@naver.com>
2022-06-14 17:21:01 +0900

    Timeline Service

--- newweek03/src/main/java/com/sparta/newweek03/Newweek03Application.java
+++ newweek03/src/main/java/com/sparta/newweek03/Newweek03Application.java
@@ -2,7 +2,10 @@ package com.sparta.newweek03;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+
+@EnableJpaAuditing //생성,수정시간 자동업데이트
 @SpringBootApplication
 public class Newweek03Application {
 
